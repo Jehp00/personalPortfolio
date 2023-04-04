14dff6b4547801beef045764b4b863c6b195bc24
Jehp00 <jehp01104006@gmail.com>
2023-04-04 17:00:39 -0500

    aodjn

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom'
-import App from '../App.jsx'
+import App from './components/App.jsx'
 import './components/styles/index.css'
 import { BrowserRouter as Router } from 'react-router-dom';
 
